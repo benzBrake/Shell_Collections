7932d822ff3a59b43e30e68abe1b04f7d83084d7
benzBrake <github-benzBrake@woai.ru>
2017-10-30 14:27:34 +0800

    Fix bug & Update features 1.Remove Dependency version selection 2.fix auto exit sometimes 3.Auto install add-ons with specify folder 4.Auto detect RHEL or Debian Release

--- Nginx_OneKey/onekey.sh
+++ Nginx_OneKey/onekey.sh
@@ -1,52 +1,35 @@
-#!/bin/bash
+#!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 NO_TEMP=/tmp/nginx_onekey
-NO_CUST=no
 NO_STREAM=no
-NO_PMIR=http://sulinux.stanford.edu/mirrors/exim/pcre
-NO_OMIR=http://mirrors.ibiblio.org/openssl/source
-NO_ZMIR=http://78.108.103.11/MIRROR/ftp/png/src/history/zlib
-NO_NVER=1.9.5
-NO_PVER=8.37
-NO_OVER=1.0.2h
-NO_ZVER=1.2.7
 NO_USER=www
 NO_GROUP=www
 NO_PATH=/etc/nginx
 NO_CONF=/etc/nginx/conf/nginx.conf
 NO_LOGP=/var/log/nginx
+NO_MODULES=/usr/src/nginx/modules
 clear
 # Root Check
-function rootness {
-if [[ $EUID -ne 0 ]]; then
-   echo "Error:This script must be run as root!" 1>&2
-   exit 1
-fi
-}
-rootness
+[[ $EUID -ne 0 ]] && { echo "Error:This script must be run as root!" 1>&2; exit 1; }
 function slogan {
 echo -n "
 #========================================================================
 # 			Nginx OneKey Install Shell
-# Version :         0.21 beta1
-# Script author :   Charisma<github-charisma@32mb.cn>
+# Version :         0.23
+# Script author :   benzBrake<github-benzbrake@woai.ru>
 # Blog :            http://blog.iplayloli.com
 # System Required : Centos/Debian/Ubuntu
-# Project url :     https://github.com/Shell_Collections/Nginx_OneKey
+# Project url :     https://github.com/benzBrake/Shell_Collections/Nginx_OneKey
 #========================================================================
-"
+" 1>&2
 }
 function startup {
 	cp -r -f /etc/rc.local /etc/rc.local_bak
-	AUTO='$NO_PATH/sbin/nginx'
-	cat /etc/rc.local|grep 'exit 0'
-	if [ $? -eq 0 ]; then
-		sed -i 's/\"exit 0\"/\#/g' /etc/rc.local
-		sed -i 's/\#exit 0/\#/g' /etc/rc.local
+	AUTO="$NO_PATH/sbin/nginx"
+	cat /etc/rc.local|grep "$AUTO"
+	if [ $? -ne 0 ]; then
 		sed -i "s#exit 0#$AUTO\nexit 0#" /etc/rc.local
-	else
-		echo "$AUTO">>/etc/rc.local
 	fi
 }
 function install {
@@ -55,12 +38,14 @@ function install {
 	else
 		configure
 	fi
+	
 #	Start Install
 	mkdir -p "$NO_TEMP"
 	install_nginx
-	exit
 #	Show Config Info
-	$NO_PATH/sbin/nginx -V
+	cd "$NO_PATH/sbin/"
+	./nginx
+	./nginx -V
 	exit
 }
 function changeversion {
@@ -68,7 +53,7 @@ function changeversion {
 	if [ -f "$HOME/nginx_onekey_config" ];then
 		source $HOME/nginx_onekey_config
 		read -p "# Please input the nginx version you want to install[1.9.5]:" changever
-		if [ ! -n "$changever" ];then
+		if [ -z "$changever" ];then
 			export NO_NVER="1.9.5"
 		else
 			export NO_NVER=$changever
@@ -80,38 +65,29 @@ function changeversion {
 		exit 2;
 	fi
 }
-function centos_nginx {
-	yum update -y || yum update -y
+function centos_prepare {
 	yum install gcc gcc-c++ make automake -y
 	if [ $? -eq 0 ]; then
 		echo "gcc gcc-c++ make automake installed"
 	else
 		yum install gcc gcc-c++ make automake -y
 	fi
-	if [ "$NO_CUST" = "no" ]; then
-		yum install pcre pcre-devel -y
-		if [ $? -eq 0 ]; then
-			echo "pcre pcre-devel installed"
-		else
-			yum install pcre pcre-devel -y
-		fi
-		yum install zlib zlib-devel openssl openssl-devel -y
-		if [ $? -eq 0 ]; then
-			echo "zlib zlib-devel openssl openssl-devel installed"
-		else
-			yum install zlib zlib-devel openssl openssl-devel -y
-		fi
+
+	yum install pcre pcre-devel -y
+	if [ $? -eq 0 ]; then
+		echo "pcre pcre-devel installed"
 	else
-		wget "$NO_PMIR/pcre-$NO_PVER.tar.gz"
-		tar -xzvf "pcre-$NO_PVER.tar.gz"
-		wget "$NO_OMIR/openssl-$NO_OVER.tar.gz"
-		tar -xzvf "openssl-$NO_OVER.tar.gz"
-		wget "$NO_ZMIR/zlib-$NO_ZVER.tar.gz"
-		tar -xzvf "zlib-$NO_ZVER.tar.gz"
+		yum install pcre pcre-devel -y
+	fi
+	yum install zlib zlib-devel openssl openssl-devel -y
+	if [ $? -eq 0 ]; then
+		echo "zlib zlib-devel openssl openssl-devel installed"
+	else
+		yum install zlib zlib-devel openssl openssl-devel -y
 	fi
 }
-function debian_nginx {
-	apt-get update || apt-get update
+function debian_prepare {
+	apt-get update -y
 	apt-get install build-essential -y
 	apt-get install git-core -y
 #	Handmade Clean and Streamlined Debian VPS System
@@ -124,27 +100,18 @@ function debian_nginx {
 	# Remove apache2
 	apt-get -y purge apache2-*
 	
-	# PCRE & OPENSSL & ZLIB
-	if [ "$NO_CUST" = "no" ]; then
+	# PCRE & OPENSSL & ZLIBn
+	apt-get install -y libpcre3 libpcre3-dev
+	if [ $? -eq 0 ]; then
+		echo "libpcre3 libpcre3-dev installed"
+	else
 		apt-get install -y libpcre3 libpcre3-dev
-		if [ $? -eq 0 ]; then
-			echo "libpcre3 libpcre3-dev installed"
-		else
-			apt-get install -y libpcre3 libpcre3-dev
-		fi
+	fi
+	apt-get install -y zlib1g zlib1g-dev openssl libssl-dev
+	if [ $? -eq 0 ]; then
+		echo "zlib1g zlib1g-dev openssl libssl-dev installed"
+	else
 		apt-get install -y zlib1g zlib1g-dev openssl libssl-dev
-		if [ $? -eq 0 ]; then
-			echo "zlib1g zlib1g-dev openssl libssl-dev installed"
-		else
-			apt-get install -y zlib1g zlib1g-dev openssl libssl-dev
-		fi
-	else 
-		wget "$NO_PMIR/pcre-$NO_PVER.tar.gz"
-		tar -xzvf "pcre-$NO_PVER.tar.gz"
-		wget "$NO_OMIR/openssl-$NO_OVER.tar.gz"
-		tar -xzvf "openssl-$NO_OVER.tar.gz"
-		wget "$NO_ZMIR/zlib-$NO_ZVER.tar.gz"
-		tar -xzvf "zlib-$NO_ZVER.tar.gz"
 	fi
 }
 function install_nginx {
@@ -152,26 +119,27 @@ function install_nginx {
 	test -d "$NO_TEMP" || mkdir -p "$NO_TEMP"
 	test -d "$NO_LOGP" || mkdir -p "$NO_LOGP"
 	cd "$NO_TEMP"
-	if [ "$NO_SYST" = "Centos" ]; then
-		centos_nginx
+	if [ -n "$(command -v yum)" ]; then
+		centos_prepare
 	else
-		debian_nginx
+		debian_prepare
 	fi
 	# Add user & group
 	/usr/sbin/groupadd -f "$NO_GROUP"
 	/usr/sbin/useradd -g "$NO_GROUP" "$NO_USER"
 	# Download Nginx and Module
-	wget "http://nginx.org/download/nginx-$NO_NVER.tar.gz" || wget "http://nginx.org/download/nginx-$NO_NVER.tar.gz"
+	wget "http://nginx.org/download/nginx-$NO_NVER.tar.gz"
 	tar -xzvf "nginx-$NO_NVER.tar.gz" || tar -xzvf "nginx-$NO_NVER.tar.gz"
-	wget -N --no-check-certificate https://raw.githubusercontent.com/Char1sma/Shell_Collections/master/Nginx_OneKey/ngx_http_substitutions_filter_module.tar.gz
-	tar -xzvf ngx_http_substitutions_filter_module.tar.gz
+	NO_OPTS=""
+	! test-z "$NO_MODULES" && test -d "$NO_MODULES" && {
+		for line in $(ls -F $NO_MODULES | grep '/$')
+		do
+			NO_OPTS="$NO_OPTS --add-module=$NO_MODULES/$(echo $line | sed 's#/$##')"
+		done
+	}
 	# Compile Nginx
 	cd "$NO_TEMP/nginx-$NO_NVER"
-	if [ "$NO_CUST" = "no" ]; then
-		./configure --prefix="$NO_PATH" --conf-path="$NO_CONF" --user="$NO_USER" --group="$NO_GROUP" --error-log-path="$NO_LOGP/error.log" --http-log-path="$NO_LOGP/access.log" --pid-path=/var/run/nginx/nginx.pid --lock-path=/var/lock/nginx.lock --with-pcre-jit --with-ipv6 --with-http_ssl_module --with-http_stub_status_module --with-http_gzip_static_module --add-module="$NO_TEMP/ngx_http_substitutions_filter_module"
-	else
-		./configure --prefix="$NO_PATH" --conf-path="$NO_CONF" --with-pcre="$NO_TEMP/pcre-$NO_PVER" --user="$NO_USER" --group="NO_GROUP" --error-log-path="$NO_LOGP/error.log" --http-log-path="$NO_LOGP/access.log" --pid-path=/var/run/nginx/nginx.pid --lock-path=/var/lock/nginx.lock --with-ipv6 --with-http_ssl_module --with-openssl="$NO_TEMP/openssl-$NO_OVER" --with-http_stub_status_module --with-http_gzip_static_module --with-zlib="$NO_TEMP/zlib-$NO_ZVER" --add-module="$NO_TEMP/ngx_http_substitutions_filter_module"
-	fi
+	./configure --prefix="$NO_PATH" --conf-path="$NO_CONF" --user="$NO_USER" --group="$NO_GROUP" --error-log-path="$NO_LOGP/error.log" --http-log-path="$NO_LOGP/access.log" --pid-path=/var/run/nginx/nginx.pid --lock-path=/var/lock/nginx.lock --with-pcre-jit --with-ipv6 --with-http_ssl_module --with-http_stub_status_module --with-http_gzip_static_module $NO_OPTS
 	# Installation
 	make
 	if [ ! $? -eq 0 ]; then
@@ -192,7 +160,6 @@ function install_nginx {
 #4.Make dir and clean
 	mkdir -p "$NO_LOGP"
 	rm -rf "$NO_TEMP"
-	exit
 }
 function configure {
 	if [ ! -f "$HOME/nginx_onekey_config" ]; then
@@ -212,77 +179,28 @@ function configure {
 	fi
 }
 function question {
-	echo -n "Select which you want:
-	1.Install for Debian/Ubuntu
-	2.Install for Centos
-Your choice:"
-	read -r syst
-	case "$syst" in
-	1)
-		NO_SYST=Debian
-		;;
-	2)
-		NO_SYST=Centos
+	read -p "# Do you want to Streamlined your VPS[y/N]:" stream
+	case "$stream" in
+	Y|y)
+		export NO_STREAM=yes
 		;;
 	*)
-		echo '# Error option, quit!'
-		exit 1
+		export NO_STREAM=no
 		;;
 	esac
-	read -p "# Do you want to custom installation info[y/N]:" custom
-		case "$custom" in
-		Y|y)
-			export NO_CUST=yes
-			read -p "# Do you want to Streamlined your VPS[y/N]:" stream
-			case "$stream" in
-			Y|y)
-				export NO_STREAM=yes
-				;;
-			*)
-				export NO_STREAM=no
-				;;
-			esac
-			read -p "# Please input the nginx version you want to install[1.9.5]:" nginx_ver
-			if [ ! "$nginx_ver" ];then
-				export NO_NVER=1.9.5
-			else 
-				export NO_NVER="$ningx_ver"
-			fi
-			read -p "# Please input the pcre version you want to install[8.37]:" pcre_ver
-			if [ ! -n "$pcre_ver" ];then
-				export NO_PVER=8.37
-			else
-				export NO_PVER="$pcre_ver"
-			fi
-			read -p "# Please input the openssl version you want to install[1.0.2h]:" ossl_ver
-			if [ ! -n "$ossl_ver" ];then
-				export NO_OVER=1.0.2h
-			else
-				export NO_OVER="$ossl_ver"
-			fi
-			read -p "# Please input the zlib version you want to install[1.2.7]:" zlib_ver
-			if [ ! -n "$zlib_ver" ];then
-				export NO_ZVER=1.2.7
-			else
-				export NO_ZVER="$zlib_ver"
-			fi
-			;;
-		*)
-			echo "# Skiped!"
-			;;
-		esac
-		write_conf
+	read -p "# Please input the nginx version you want to install[1.9.5]:" nginx_ver
+	if [ ! "$nginx_ver" ];then
+		export NO_NVER=1.9.5
+	else 
+		export NO_NVER="$ningx_ver"
+	fi
+	write_conf
 }
 function write_conf {
 	cat >> "$HOME/nginx_onekey_config" << EOF
-NO_SYST=$NO_SYST
 NO_TEMP=$NO_TEMP
-NO_CUST=$NO_CUST
 NO_NVER=$NO_NVER
-NO_PVER=$NO_PVER
 NO_STREAM=$NO_STREAM
-NO_OVER=$NO_OVER
-NO_ZVER=$NO_ZVER
 NO_USER=$NO_USER
 NO_GROUP=$NO_GROUP
 NO_PATH=$NO_PATH
